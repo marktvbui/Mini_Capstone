c5ae27343da5b4bd84b66101bafcadd821adbcf8
Mark Bui <tvyybui@yahoo.com>
2016-11-17 20:32:31 -0800

    changed price attribute to decimal

---
+++ db/migrate/20161118041910_changing_price_attribute.rb
@@ -0,0 +1,5 @@
+class ChangingPriceAttribute < ActiveRecord::Migration[5.0]
+  def change
+    change_column :products, :price, 'decimal USING CAST(price AS decimal)', precision: 10, scale: 2 
+  end
+end
