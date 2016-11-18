48b09d9417fd94ab07a19d2508c3607e580b660c
Mark Bui <tvyybui@yahoo.com>
2016-11-17 19:52:18 -0800

    added modules: tax, total, sale_item to app

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -29,7 +29,6 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find_by(id: params[:id])
-    @list_of_ingredients = @products.xx.split(", ")
   end
 
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,24 @@ class Product < ApplicationRecord
     return created_at.strftime("%A, %d %b %Y %l:%M %p")
   end
 
+  def sale_message
+    if price.to_f < 250
+      return "Discount Item!"
+    else
+      return "On Sale!"
+    end
+
+  end
+
+  def tax
+    return price.to_f * 0.09
+  end
+
+  def total
+    return tax + price.to_f
+  end
+
+
 
 
 
